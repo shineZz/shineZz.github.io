173f8db45d524a3411d030c30a35ccf2aeb21d94
shineZz <714165655@qq.com>
2023-01-08 13:10:40 +0000

    Deploying to gh-pages from @ shineZz/shineZz.github.io.source@34e4b7894ebdf1a197790af26892ddf98e1bb5e9 🚀

---
+++ precache-manifest.23404cbba61a9f8c47a3575be5b4cc59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702e55e60a75dbec3266c5fa3e361801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0fcb614c0c7123e1af5",
+    "url": "/static/css/2.7bf1e265.chunk.css"
+  },
+  {
+    "revision": "dbbc6f98924efa60897d",
+    "url": "/static/css/main.ad9776cf.chunk.css"
+  },
+  {
+    "revision": "a0fcb614c0c7123e1af5",
+    "url": "/static/js/2.91031fa5.chunk.js"
+  },
+  {
+    "revision": "dbbc6f98924efa60897d",
+    "url": "/static/js/main.8b918e93.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4abbb3d84d6cc38507aa6a72563c678b",
+    "url": "/static/media/avatar.4abbb3d8.jpg"
+  }
+]);
